0ca579e08ccff7e432a56ffef048405175fbaec6
Tareq-Bilal <tareqbilal44@gmail.com>
2026-06-19 23:51:56 +0300

    Change Mileage property type from decimal to int in Item entity and update migration files

---
+++ src/AuctionService/Migrations/20260619204844_ChangeMileageToInt.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeMileageToInt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Mileage",
+                table: "Items",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Mileage",
+                table: "Items",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
