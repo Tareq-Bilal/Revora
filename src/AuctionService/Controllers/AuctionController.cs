022af89518287bceaaae4502d3b850668ec2493b
Tareq-Bilal <tareqbilal44@gmail.com>
2026-06-20 01:53:42 +0300

    Add Auction API with DTOs and AutoMapper configuration

---
+++ src/AuctionService/Controllers/AuctionController.cs
@@ -0,0 +1,55 @@
+using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.Entites;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuctionController : ControllerBase
+{
+    private readonly AuctionDbContext _context;
+    private readonly IMapper _mapper;
+
+    public AuctionController(AuctionDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> ListAuctions()
+    {
+        var auctions = await _context.Auctions
+        .Include(a => a.Item)
+        .OrderBy(a => a.Item.Make)
+        .ToListAsync();
+
+        return _mapper.Map<List<AuctionDto>>(auctions);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDto>> GetAuction(Guid id)
+    {
+        var auction = await _context.Auctions
+        .Include(a => a.Item)
+        .FirstOrDefaultAsync(a => a.Id == id);
+
+        if (auction == null) return NotFound();
+
+        return _mapper.Map<AuctionDto>(auction);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction([FromBody] CreateAuctionDto createAuctionDto)
+    {
+        var auction = _mapper.Map<Auction>(createAuctionDto);
+        _context.Auctions.Add(auction);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetAuction), new { id = auction.Id }, auction);
+    }
+
+}
---
+++ src/AuctionService/RerquestHelpers/MappingProfiles.cs
@@ -0,0 +1,17 @@
+using AuctionService.DTOs;
+using AuctionService.Entites;
+using AutoMapper;
+
+namespace AuctionService.RerquestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {                                   //means: “When mapping Auction to AuctionDto, also use Auction.Item as a source.”
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+        CreateMap<CreateAuctionDto, Auction>()
+        .ForMember(dest => dest.Item, opt => opt.MapFrom(src => src));
+        CreateMap<CreateAuctionDto, Item>();
+    }
+}   
