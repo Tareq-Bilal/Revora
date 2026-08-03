592370ba865905a36d2863d10d44b3cdd53dd94b
Tareq-Bilal <tareqbilal44@gmail.com>
2026-08-03 11:39:42 +0300

    Add AuctionFinished and BidPlaced contract classes

---
+++ src/Contracts/AuctionFinished.cs
@@ -0,0 +1,11 @@
+namespace Contracts;
+
+public class AuctionFinished
+{
+    public bool ItemSold { get; set; }
+    public Guid AuctionId { get; set; }
+    public string Winner { get; set; }
+    public string Seller { get; set; }
+    public int? SoldAmount { get; set; }
+
+}
